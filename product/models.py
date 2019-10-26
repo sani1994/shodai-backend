9bb810298e753a49fa9e93a6ee7da1645e702bc8
Sani khan <sanikhan2016@gmail.com>
2019-10-26 22:28:19 +0600

    error fixed

--- product/models.py
+++ product/models.py
@@ -35,7 +35,7 @@ class ProductMeta(BaseModel): # Prodect Meta (original product name with comapny
 class Product(BaseModel):
     # put retailer as foreign key
     product_name = models.CharField(max_length=100, blank=True, null=True)
-    product_image = models.ImageField(upload_to='pictures/product/%Y/%m/%d/', blank=True, null=True)
+    product_image = models.ImageField(upload_to='pictures/product/', blank=True, null=True)
     product_unit = models.CharField(max_length=3, blank=True, null=True)
     product_price = models.DecimalField(decimal_places=2,max_digits=7,blank=True, null=True)
     # product_offer_price = models.DecimalField(decimal_places=2,max_digits=7,blank=True,null=True)
