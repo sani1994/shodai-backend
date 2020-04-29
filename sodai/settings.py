1be4e4035b25e297e0b82cfbc9ed509f1968c046
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-04-29 16:30:19 +0600

    update static setting

--- sodai/settings.py
+++ sodai/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+
 AUTH_USER_MODEL = 'userProfile.UserProfile'
 
 REST_FRAMEWORK = {
@@ -114,16 +115,16 @@ WSGI_APPLICATION = 'sodai.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#         'NAME': 'shodai',
-#         'USER': 'admin',
-#         'PASSWORD': '',
-#         'HOST': 'localhost'
-#     }
-# }
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'shodai',
+        'USER': 'admin',
+        'PASSWORD': '',
+        'HOST': 'localhost'
+    }
+}
 
 
 # DATABASES = {
--- sodai/urls.py
+++ sodai/urls.py
@@ -35,13 +35,23 @@ from material.admin.sites import site
 site.site_header = _('Shodai')
 site.site_title = _('Shodai')
 # site.favicon = staticfiles('../media/')
-site.main_bg_color = 'skyblue'
+site.main_bg_color = '#088A3F'
 site.main_hover_color = 'black'
-site.profile_picture = staticfiles('../media/shodai.jpg')
-site.profile_bg = staticfiles('../media/white.jpg')
-site.login_logo = staticfiles('../media/shodai.jpg')
-site.logout_bg = staticfiles('../media/white.jpg')
-site.login_bg_color = 'skyblue'
+
+# site.profile_picture = staticfiles('../media/shodai.jpg')
+site.profile_picture = "https://lh3.googleusercontent.com/hJ504D-hcWebdHJw1WhKixKaC1rj4JL31mB5l1vSX6Tmj6IV7lIRv5rUy-2PWKe4hUzTJazSkCPByyfMaBR9nPJMdmsrJYxa1BOzyvgVguZLWu0mjsnLciAv365wCjyOXW-xyUpLMUZJv9iZUZbtzNool0pcYYyWIVbPQsGsAn4NPfwgEjy7d6_YInJbErp1zzrrP4PgPyr2fDX4rivZBjM2w9nzAxakPb-xJK_s2bTWzkymvLcUsBWL3bByUqdz6MtLFTmR9o-IL56iG0QuCWLJpwdXEu4qwKSfwA2DGc1NF_WDvrSKIgBau6BkZOYbxCd4EICGGfsLpCSf6JOMAohNuY4eupnU4gf4bP8c9cQVYMWyPKfisgFzCN-BUOw1j-loBSP9DbCpCCkUFJcpsvGE_MgxctToORee8CB9576HOceHy0NB_sD0Xbw-O9WTePTf4MwsOzhyUGKDg3KK8001PmSDkNLqwHNDMdKBJ9Xl_jByHFqh_8h-TA8rjrFATsGfwVpfoRDAw6MnDAP40n-XN6_5E4DgT6GodYoUZigjJPp806YiejPU6Vim5SYQpPUF0PBA74Nwp1Ji1F9pC1yEw4Yn07QkI1anv0Mha-TpipLUHTWu9UkeCePp3CCJxLlPiHEJnHKiCaa2pf3aSa_L4olN55J38yR4BVGmE6acjGNU_JTNpFN5e3sp_A=w795-h789-no"
+
+
+site.profile_bg = "https://lh3.googleusercontent.com/9RyrRW867Rtm-g9uJELf0NwsXLBdugHZOc4T8YsBK5fT81nQzvfOwJF0cpigXJ5q_RJHRcmF0Xs86uab1fohaIa_pyNfL7XeiYL55Gc182nHyP4tmgxUej_LHD6f8h3VZTOWlgFuO_Lgb-742m0478tu2qQHm2zV2HqBOmQCBtIWJFXU8aNCvesDrPncPOboYKEm9tMWf2gyxyGhTOPde7SfxV4eKHvkQNujQhj5rdUELhHyZQy9I4QBqFI3LSZARdSThjSYOsm6brrHP043Ok1uIAqs0fj2a-06mi3Y7hCK1tEMfPvKf1ppaq-nyhejcYkePjbBugY7SFuHN3lzLbB7sgp6G6iyIYnNz47mG1jomlSBWScP4aYLlIatNGEo_1UfZreVgpioGStCoJ6TUzuHoH2fyRYvt5i0-MxS4-5fyqs_eWtmu63npELXpn8r8LMprlZFSfdO087bIhWixuojt758FP0fzMfTn1uMRImBI0-dGC4VrLXWceRwErvG4_18t_EZAVhbVVBa2DjERMazOQDaX_2YQm55ez3_WwdkhEqDd2KPKtanVDAqBHqALGJzFckr4iaDD9JOum0rJx1qlLvQfejSAOwnzWFZmApC7ZI_1Lt2T5yqFVFsx2IjiDt2qMJ-vHkAQ733Dl2jAXErJzzebeJ4PyU9mUE_s6o3_-WA7IvkzBzEM7VZGw=w1019-h789-no"
+# site.profile_bg = staticfiles('../media/white.jpg')
+
+# site.login_logo = staticfiles('../media/shodai.jpg')
+site.login_logo = "https://lh3.googleusercontent.com/hJ504D-hcWebdHJw1WhKixKaC1rj4JL31mB5l1vSX6Tmj6IV7lIRv5rUy-2PWKe4hUzTJazSkCPByyfMaBR9nPJMdmsrJYxa1BOzyvgVguZLWu0mjsnLciAv365wCjyOXW-xyUpLMUZJv9iZUZbtzNool0pcYYyWIVbPQsGsAn4NPfwgEjy7d6_YInJbErp1zzrrP4PgPyr2fDX4rivZBjM2w9nzAxakPb-xJK_s2bTWzkymvLcUsBWL3bByUqdz6MtLFTmR9o-IL56iG0QuCWLJpwdXEu4qwKSfwA2DGc1NF_WDvrSKIgBau6BkZOYbxCd4EICGGfsLpCSf6JOMAohNuY4eupnU4gf4bP8c9cQVYMWyPKfisgFzCN-BUOw1j-loBSP9DbCpCCkUFJcpsvGE_MgxctToORee8CB9576HOceHy0NB_sD0Xbw-O9WTePTf4MwsOzhyUGKDg3KK8001PmSDkNLqwHNDMdKBJ9Xl_jByHFqh_8h-TA8rjrFATsGfwVpfoRDAw6MnDAP40n-XN6_5E4DgT6GodYoUZigjJPp806YiejPU6Vim5SYQpPUF0PBA74Nwp1Ji1F9pC1yEw4Yn07QkI1anv0Mha-TpipLUHTWu9UkeCePp3CCJxLlPiHEJnHKiCaa2pf3aSa_L4olN55J38yR4BVGmE6acjGNU_JTNpFN5e3sp_A=w795-h789-no"
+
+# site.logout_bg = staticfiles('../media/white.jpg')
+site.logout_bg = "https://lh3.googleusercontent.com/9RyrRW867Rtm-g9uJELf0NwsXLBdugHZOc4T8YsBK5fT81nQzvfOwJF0cpigXJ5q_RJHRcmF0Xs86uab1fohaIa_pyNfL7XeiYL55Gc182nHyP4tmgxUej_LHD6f8h3VZTOWlgFuO_Lgb-742m0478tu2qQHm2zV2HqBOmQCBtIWJFXU8aNCvesDrPncPOboYKEm9tMWf2gyxyGhTOPde7SfxV4eKHvkQNujQhj5rdUELhHyZQy9I4QBqFI3LSZARdSThjSYOsm6brrHP043Ok1uIAqs0fj2a-06mi3Y7hCK1tEMfPvKf1ppaq-nyhejcYkePjbBugY7SFuHN3lzLbB7sgp6G6iyIYnNz47mG1jomlSBWScP4aYLlIatNGEo_1UfZreVgpioGStCoJ6TUzuHoH2fyRYvt5i0-MxS4-5fyqs_eWtmu63npELXpn8r8LMprlZFSfdO087bIhWixuojt758FP0fzMfTn1uMRImBI0-dGC4VrLXWceRwErvG4_18t_EZAVhbVVBa2DjERMazOQDaX_2YQm55ez3_WwdkhEqDd2KPKtanVDAqBHqALGJzFckr4iaDD9JOum0rJx1qlLvQfejSAOwnzWFZmApC7ZI_1Lt2T5yqFVFsx2IjiDt2qMJ-vHkAQ733Dl2jAXErJzzebeJ4PyU9mUE_s6o3_-WA7IvkzBzEM7VZGw=w1019-h789-no"
+
+site.login_bg_color = '#088A3F'
 # site.ordering = ('userProfile','Product')
 ##################################################
 
