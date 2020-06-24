9094d502708b3fb7b1d11bafdf9f0a943aa8b44d
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-06-24 12:35:50 +0600

    update order api and TimeSlot err

--- order/views.py
+++ order/views.py
@@ -5,7 +5,7 @@ from rest_framework.generics import get_object_or_404
 from order.serializers import OrderSerializer, OrderProductSerializer, VatSerializer, OrderProductReadSerializer, \
     DeliveryChargeSerializer, PaymentInfoDetailSerializer, PaymentInfoSerializer, OrderDetailSerializer, \
     OrderDetailPaymentSerializer #TimeSlotSerializer
-from order.models import OrderProduct, Order, Vat, DeliveryCharge, PaymentInfo, #TimeSlot
+from order.models import OrderProduct, Order, Vat, DeliveryCharge, PaymentInfo #TimeSlot
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
