44f3312b3a554a7d526a8aa65fee2d5b0d392cea
Reaz Nahid <reaznahid@finder-lbs.com>
2020-07-12 18:05:45 +0600

    sms sending service updated

--- utility/notification.py
+++ utility/notification.py
@@ -7,16 +7,34 @@ otp_text = "{} - OTP for Shodai Login. Valid for next five (5) minutes."
 
 
 def email_notification(subject, body):
-    # target_user = ['rana@shod.ai', 'shishir@shod.ai', 'support@shod.ai', 'mohua@finder-lbs.com']
     target_user = config("TARGET_EMAIL_USER").replace(" ", "").split(',')
     return send_mail(subject, body, settings.EMAIL_HOST_USER, target_user)
 
 
 def send_sms(mobile_number, sms_content):
-    url = "https://portal.adnsms.com/api/v1/secure/send-sms"
+    url = config("SMS_API_URL", None)
     body = {
-        "api_key": config("OTP_API_KEY", None),
-        "api_secret": config("OTP_SECRET", None),
+        "api_key": config("SMS_API_KEY", None),
+        "api_secret": config("SMS_API_SECRET", None),
+        "request_type": "single_sms",
+        "message_type": "Text",
+        "mobile": mobile_number,
+        "message_body": sms_content
+    }
+
+    response = post(url, data=body).json()
+
+    if response["api_response_message"] == "SUCCESS":
+        return 'success'
+    else:
+        return 'failed'
+
+
+def send_sms_otp(mobile_number, sms_content):
+    url = config("SMS_API_URL", None)
+    body = {
+        "api_key": config("SMS_API_KEY", None),
+        "api_secret": config("SMS_API_SECRET", None),
         "request_type": "OTP",
         "message_type": "Text",
         "mobile": mobile_number,
