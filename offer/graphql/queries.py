45a8286bdd190f0d8819b5f5c4e3e3594aff0c31
Booshra <booshra@finder-lbs.com>
2020-10-28 17:32:54 +0600

    offer product pagination added

--- offer/graphql/queries.py
+++ offer/graphql/queries.py
@@ -4,7 +4,9 @@ import graphene
 from graphene_django.types import DjangoObjectType
 
 from bases.views import checkAuthentication
+from product.graphql.queries import ProductConnection
 from ..models import Offer, OfferProduct
+from graphene import relay
 
 
 class OfferType(DjangoObjectType):
@@ -17,17 +19,39 @@ class OfferProductType(DjangoObjectType):
         model = OfferProduct
 
 
+class OfferProductNode(DjangoObjectType):
+    class Meta:
+        model = OfferProduct
+        interfaces = (relay.Node,)
+
+    # @classmethod
+    # def get_queryset(cls, queryset, info):
+    #     return queryset.filter(is_approved=True)
+
+
+class OfferProductConnection(relay.Connection):
+    class Meta:
+        node = OfferProductNode
+
+
 class Query(graphene.ObjectType):
     offer_list = graphene.List(OfferType)
-    offer_product_list = graphene.List(OfferProductType)
+    all_offer_products = relay.ConnectionField(ProductConnection)
+    offer_product_pagination = relay.ConnectionField(OfferProductConnection)
 
     def resolve_offer_list(self, info):
         today = timezone.now()
         all_offers = Offer.objects.filter(is_approved=True, offer_starts_in__lte=today, offer_ends_in__gte=today)
         return all_offers
 
-    def resolve_offer_product_list(self, info):
+    def resolve_all_offer_products(root, info):
         today = timezone.now()
         all_offer_products = OfferProduct.objects.filter(is_approved=True, offer__offer_starts_in__lte=today,
                                                          offer__offer_ends_in__gte=today)
-        return all_offer_products
+        products = [op.product for op in all_offer_products]
+        return products
+
+    def resolve_offer_product_pagination(root, info, **kwargs):
+        today = timezone.now()
+        return OfferProduct.objects.filter(is_approved=True, offer__offer_starts_in__lte=today,
+                                           offer__offer_ends_in__gte=today).order_by('-created_on')
